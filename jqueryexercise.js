ff1f6922eee0d929306c47317d52928a24c5a9f5
EricMoxley <exilerzero@yahoo.com>
2019-09-10 17:00:47 -0400

    jQuery exercise
    learning to use jQuery selectors to change html with .css methods.

---
+++ jqueryexercise.js
@@ -0,0 +1,8 @@
+//change all divs to have background purple
+$("div").css("backgroundColor", "purple");
+//give selected highlight divs range 
+$("div.highlight").css("width", "200px");
+//select div with id (#) third and give it orange border
+$("#third").css("border", "4px solid orange");
+
+$("div:first-of-type").css("color", "pink");
